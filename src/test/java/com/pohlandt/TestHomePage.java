7287ab2aaefe07d1092a2d07bc9e1bda7a2037c6
pohlandt <pohlandt@gmail.com>
2014-09-27 00:03:27 +0200

    jndi setup for tests

--- src/test/java/com/pohlandt/TestHomePage.java
+++ src/test/java/com/pohlandt/TestHomePage.java
@@ -1,8 +1,15 @@
 package com.pohlandt;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 
 /**
  * Simple test using the WicketTester
@@ -11,6 +18,34 @@ public class TestHomePage
 {
 	private WicketTester tester;
 
+	 @BeforeClass
+    public static void setUpClass() throws Exception {
+        try {
+            // Create initial context
+            System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+                "org.apache.naming.java.javaURLContextFactory");
+            System.setProperty(Context.URL_PKG_PREFIXES, 
+                "org.apache.naming");            
+            InitialContext ic = new InitialContext();
+
+            ic.createSubcontext("java:");
+            ic.createSubcontext("java:/comp");
+            ic.createSubcontext("java:/comp/env");
+            ic.createSubcontext("java:/comp/env/jdbc");
+           
+            // Construct DataSource
+            MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
+            ds.setURL("jdbc:mysql://pohlandt.net:8003/wicketproto");
+            ds.setUser("wicketproto");
+            ds.setPassword("itsjustaprotomon");
+            
+            ic.bind("java:/comp/env/jdbc/wicketproto", ds);
+        } catch (NamingException ex) {
+            System.out.println(ex);
+        }
+        
+    }
+	
 	@Before
 	public void setUp()
 	{
