e3e1d63f172a125191626fbb198afe52c560c8c5
Jonas Pohlandt <jonas.pohlandt@sinnerschrader.com>
2015-06-02 11:26:45 +0200

    dependency updates to enable testability via maven wicket 6.17 -> 6.19

--- src/test/java/com/pohlandt/test/TestSecurityModule.java
+++ src/test/java/com/pohlandt/test/TestSecurityModule.java
@@ -4,12 +4,14 @@ import org.apache.shiro.authc.credential.CredentialsMatcher;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.shiro.guice.ShiroModule;
+import org.apache.shiro.util.ByteSource;
 import org.slf4j.Logger;
 
 import com.google.inject.name.Names;
 import com.pohlandt.entity.IEntityRepository;
 import com.pohlandt.security.EntityRealm;
 import com.pohlandt.security.EntitySecurityManager;
+import com.pohlandt.security.IHasher;
 import com.pohlandt.security.ISecurityManager;
 
 public class TestSecurityModule extends ShiroModule {
@@ -25,7 +27,14 @@ public class TestSecurityModule extends ShiroModule {
 		bind(CredentialsMatcher.class).to(HashedCredentialsMatcher.class);
 	    bind(HashedCredentialsMatcher.class);
 	    bindConstant().annotatedWith(Names.named("shiro.hashAlgorithmName")).to(Sha256Hash.ALGORITHM_NAME);
+	    bind(IHasher.class).toInstance(new IHasher() {
+			@Override
+			public String hash(String text, ByteSource salt) {
+				return new Sha256Hash(text, salt).toHex();
+			}
+		});
 	    bind(ISecurityManager.class).to(EntitySecurityManager.class);
+	    expose(ISecurityManager.class);
 	}
 
 }
